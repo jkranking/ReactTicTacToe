cb00a9dc53ef0504772ad8c1038255204d137f1b
Jeff Kranking <jkranking@gmail.com>
2017-02-22 15:47:18 -0800

    creates announcement, reset and tile files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
+import './Tile.jsx';
+import './Announcement.jsx';
+import './ResetButton.jsx';
 
 class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			gameBoard: [
+			 ' ', ' ', ' ',
+			 ' ', ' ', ' ',
+			 ' ', ' ', ' '
+			]
+		}
+	}
+
+
+
   render() {
     return (
       <div className="container">
-      	
+      	<div className="menu">
+      		<h1>Tic-Tac-Toe</h1>
+      		<Announcement />
+      		<ResetButton />
+      	</div>
+      	{this.state.gameBoard.map(function(value, i){
+      		<Tile/>
+      	})}
       </div>
 
     );
