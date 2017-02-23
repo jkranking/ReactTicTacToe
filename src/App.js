4735186a358f45b8fc1633dc17ad820b1498725f
Jeff Kranking <jkranking@gmail.com>
2017-02-22 17:43:05 -0800

    finishes reset board and winning conditionals

--- src/App.js
+++ src/App.js
@@ -19,9 +19,72 @@ class App extends Component {
 	}
 
 updateBoard(loc, player){
+	if(this.state.gameBoard[loc] === 'x' || this.state.gameBoard[loc] === 'o'){
+		//Invalid move
+	return;
+	}
+	let currentGameBoard = this.state.gameBoard;
+	currentGameBoard.splice(loc, 1, this.state.turn);
+	this.setState({gameBoard: currentGameBoard});
+
+
+	let topRow = this.state.gameBoard[0] + this.state.gameBoard[1] + this.state.gameBoard[2];
+	if (topRow.match(/xxx|ooo/)) {
+		this.setState({winner: this.state.turn});
+		return;
+	}
+	let middleRow = this.state.gameBoard[3] + this.state.gameBoard[4] + this.state.gameBoard[5];
+	if (middleRow.match(/xxx|ooo/)) {
+		this.setState({winner: this.state.turn});
+		return;
+	}
+	let bottomRow = this.state.gameBoard[6] + this.state.gameBoard[7] + this.state.gameBoard[8];
+	if (bottomRow.match(/xxx|ooo/)) {
+		this.setState({winner: this.state.turn});
+		return;
+	}
+	let leftCol = this.state.gameBoard[0] + this.state.gameBoard[3] + this.state.gameBoard[6];
+	if (leftCol.match(/xxx|ooo/)) {
+		this.setState({winner: this.state.turn});
+		return;
+	}
+	let middleCol = this.state.gameBoard[1] + this.state.gameBoard[4] + this.state.gameBoard[7];
+	if (middleCol.match(/xxx|ooo/)) {
+		this.setState({winner: this.state.turn});
+		return;
+	}
+	let rightCol = this.state.gameBoard[2] + this.state.gameBoard[5] + this.state.gameBoard[8];
+	if (rightCol.match(/xxx|ooo/)) {
+		this.setState({winner: this.state.turn});
+		return;
+	}
+	let leftDiag = this.state.gameBoard[0] + this.state.gameBoard[4] + this.state.gameBoard[8];
+	if (leftDiag.match(/xxx|ooo/)) {
+		this.setState({winner: this.state.turn});
+		return;
+	}
+	let rightDiag = this.state.gameBoard[2] + this.state.gameBoard[4] + this.state.gameBoard[6];
+	if (rightDiag.match(/xxx|ooo/)) {
+		this.setState({winner: this.state.turn});
+		return;
+	}
+
+	let moves = this.state.gameBoard.join('').replace(/ /g, '');
+	if(moves.length === 9) {
+		this.setState({winner: 'd'});
+	}
 }
 
 resetBoard(){
+	this.setState({
+		gameBoard: [
+		 ' ', ' ', ' ',
+		 ' ', ' ', ' ',
+		 ' ', ' ', ' '
+		],
+		turn: 'x',
+		winner: 'null'
+	})
 }
 
   render() {
